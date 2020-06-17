431fc2a2e9439961e4f71693c1878e9b684d7287
hyacinth <yaoxu.mail@qq.com>
2020-06-17 16:45:19 +0800

    to study

--- codes/analysis-visualization/analyze_pycode/torch_lstm.py
+++ codes/analysis-visualization/analyze_pycode/torch_lstm.py
@@ -8,6 +8,7 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
 import time
 import random
+from tensorboardX import SummaryWriter
 
 
 class LSTM (nn.Module):
@@ -15,7 +16,7 @@ class LSTM (nn.Module):
         super ().__init__ ()
         self.hidden_layer_size = hidden_layer_size
 
-        self.lstm = nn.LSTM (input_size, hidden_layer_size)
+        self.lstm = nn.LSTM (input_size, hidden_layer_size, batch_first=HyperParameter["batch_first"])
 
         self.linear = nn.Linear (hidden_layer_size, output_size)
 
@@ -23,9 +24,9 @@ class LSTM (nn.Module):
                             torch.zeros (1, 1, self.hidden_layer_size))
 
     def forward(self, input_seq):
-        lstm_out, self.hidden_cell = self.lstm (input_seq.view (len (input_seq), 1, -1), self.hidden_cell)
-        predictions = self.linear (lstm_out.view (len (input_seq), -1))
-        return predictions[-1]
+        lstm_out, self.hidden_cell = self.lstm (input_seq, self.hidden_cell)
+        predictions = self.linear (lstm_out)
+        return predictions[0][-1]
 
 
 def save_model(model, epoch):
@@ -37,25 +38,30 @@ def save_model(model, epoch):
 def load_model(model, filename):
     model.load_state_dict (torch.load (f"{Model_Folder}/{filename}"))
 
+
 def evalute(model, test_seq):
-    model.eval()
+    model.eval ()
     correct = 0
-    total = len(test_seq)
+    total = len (test_seq)
     for x, y in test_seq:
-        with torch.no_grad():
-            logits = model(x).reshape(1,-1)
-            pred = logits.argmax(dim=1)
-        correct += torch.eq(pred, y).sum().float().item()
-    return correct / total # acc
+        with torch.no_grad ():
+            logits = model (x).reshape (1, -1)
+            pred = logits.argmax (dim=1)
+        correct += torch.eq (pred, y).sum ().float ().item ()
+    return correct / total  # acc
+
 
 def train_model(train_inout_seq, model, epochs=150):
-    val_list = pd.DataFrame (columns=["loss","val_acc"], index=range (epochs))
+    val_list = pd.DataFrame (columns=["loss", "val_acc"], index=range (epochs))
     best_epoch = 0
     best_acc = 0
-    for i in range (epochs):
+    for epoch in range (epochs):
         total = len (train_inout_seq)
         fi = 0
+        num_correct = 0
+        loss = 0
         for seq, labels in train_inout_seq:
+            seq = seq.unsqueeze (0)
             optimizer.zero_grad ()
             model.hidden_cell = (torch.zeros (1, 1, model.hidden_layer_size),
                                  torch.zeros (1, 1, model.hidden_layer_size))
@@ -64,19 +70,23 @@ def train_model(train_inout_seq, model, epochs=150):
             single_loss = loss_function (y_pred, labels)
             single_loss.backward ()
             optimizer.step ()
+            num_correct += torch.eq (y_pred, labels).sum ().float ().item ()
+            loss += single_loss.item ()
 
             fi += 1
-            print (f"\repoch:{i:3} training {fi / total:3.2%}", end="")
-        val_acc = evalute (model, test_seq)
+            print (f"\repoch:{epoch:3} training {fi / total:3.2%}", end="")
+        val_acc = num_correct / total
         if val_acc > best_acc:
-            best_epoch = i
+            best_epoch = epoch
             best_acc = val_acc
-        save_model (model, i)
-        val_list.iloc[i, 0] = single_loss.item ()
-        val_list.iloc[i,1] = val_acc
-        print (f'\tepoch: {i:3} loss: {single_loss.item ():6.8e}')
-        val_list.plot ()
-        plt.show ()
+        save_model (model, epoch)
+        val_list.iloc[epoch, 0] = single_loss.item ()
+        val_list.iloc[epoch, 1] = val_acc
+        writer.add_scalar ("Train_loss", loss, epoch)
+        if epoch % 1 == 0:
+            print (f'\tepoch: {epoch:3} loss: {loss:6.8e} acc : {val_acc}')
+        # writer.add_graph (model, (inputs,))
+    # writer.add_graph (model, train_inout_seq[0][0].reshape (1, HyperParameter["input_size"]))
     val_list.to_csv (f"{Model_Folder}/{time.strftime ('%Y%m%d%H%M%s')}_loss_log.csv")
     return best_epoch
 
@@ -115,28 +125,30 @@ def create_inout_sequences(input_data, tw):
     return inout_seq
 
 
-def series_process(normalized_data,inout_seq):
+def series_process(normalized_data, inout_seq):
     normalized_data = torch.FloatTensor (normalized_data)
-    inout_seq.extend(create_inout_sequences (normalized_data, HyperParameter["train_window"]))
+    inout_seq.extend (create_inout_sequences (normalized_data, HyperParameter["train_window"]))
+
     return inout_seq
 
 
 def pre_process(data):
     inout_seq = []
     data = data_normalized (data)
-    aids = list(set(data[:,0]))
+    aids = list (set (data[:, 0]))
     for aid in aids:
-        single_data = data[data[:,0] == aid]
-        if len(single_data) < HyperParameter["train_window"]:
+        single_data = data[data[:, 0] == aid]
+        if len (single_data) < HyperParameter["train_window"]:
             continue
-        series_process(single_data[:,1:],inout_seq)
+        series_process (single_data[:, 1:], inout_seq)
     train_seq_num = int (len (inout_seq) * 8 / 10)
     train_seq = inout_seq[0:train_seq_num]
     test_seq = inout_seq[train_seq_num:]
     return train_seq, test_seq
 
 
-HyperParameter = {"train_window": 5, "epochs": 200, "hidden_layer_sizes": 16, "input_size": 8, "output_size": 8}
+HyperParameter = {"train_window": 5, "epochs": 300, "hidden_layer_sizes": 180, "input_size": 8, "output_size": 8,
+                  "batch_first": True}
 Model_Folder = "./model_out"
 if __name__ == '__main__':
     filepath = '../../crawl-preprocess-storage/storage/vresult.csv'
@@ -144,49 +156,58 @@ if __name__ == '__main__':
     data = data.iloc[:, 0:9]
     # 数据再次预处理为lstm的标准序列格式
     train_seq, test_seq = pre_process (data)
+    shape = train_seq[0][0].shape
+    # for i in range (1, len (train_seq)):
+    #     b = train_seq[i][0].unsqueeze (0)
+    #     a = torch.cat ([a, b], dim=0)
+    inputs = torch.from_numpy(np.random.rand(1,shape[0],shape[1])).type(torch.FloatTensor)
 
     mode = 0
     if mode == 0:  # 训练模式
+        writer = SummaryWriter (comment="test_model", log_dir="./scalar")  # 初始化tensorboard
+
         # 初始化模型 使用交叉熵损失;使用adam优化器。
         model = LSTM (hidden_layer_size=HyperParameter["hidden_layer_sizes"],
                       input_size=HyperParameter["input_size"],
                       output_size=HyperParameter["output_size"])
         loss_function = nn.MSELoss ()
         optimizer = torch.optim.Adam (model.parameters (), lr=0.001)
+
         try:
             # 训练模型
             best_epoch = train_model (train_seq, model, epochs=HyperParameter["epochs"])
             mode = 1  # 切换模式
         except Exception as e:
             print (e)
+
+        writer.close ()
     if mode == 1:  # 预测模式
         # model_params_file_name = input ("输入保存的参数文件名:")
-        model_params_file_name = "2020061700491592326197_1_params.pkl"
+        model_params_file_name = "2020061715151592378138_1_params.pkl"
         model = LSTM (hidden_layer_size=HyperParameter["hidden_layer_sizes"],
                       input_size=HyperParameter["input_size"],
                       output_size=HyperParameter["output_size"])
         loss_function = nn.MSELoss ()
         optimizer = torch.optim.Adam (model.parameters (), lr=0.001)
-        load_model(model,model_params_file_name)
+        load_model (model, model_params_file_name)
 
-        acc = evalute(model,test_seq)
-
-        actual_predictions = model_predict (len (test_data), test_inputs, scaler)
-
-        MSE = mean_squared_error (actual_predictions, test_data.values)
-        print (f"测试集均方误差为：{MSE:10.10e}")
-        MSE_data = pd.DataFrame ({'MSE': MSE}, index=[index]).to_csv ('./MSE_data.csv', index=False, mode='a',
-                                                                      encoding='utf8')
-        # 画图
-        plt.plot (train_data)
-        plt.plot (test_data)
-        plt.plot (pred)
-
-        # 目标值预测
-        pred_inputs = normaled_test_data[-train_window:].tolist ()
-        actual_predictions_fore = model_predict (4, pred_inputs, scaler_test)
-        forecast_data[index] = actual_predictions_fore
-        plt.plot (forecast_data[index])
-        plt.savefig (index + '.png')
-        plt.show ()
-    forecast_data.to_csv ('./forecast_data.csv', index=False, mode='w', encoding='utf8')
+        acc = evalute (model, test_seq)
+        #
+        # actual_predictions = model_predict (len (test_data), test_inputs, scaler)
+        #
+        # MSE = mean_squared_error (actual_predictions, test_data.values)
+        # print (f"测试集均方误差为：{MSE:10.10e}")
+        # MSE_data = pd.DataFrame ({'MSE': MSE}, index=[index]).to_csv ('./MSE_data.csv', index=False, mode='a',
+        #                                                               encoding='utf8')
+        # # 画图
+        # plt.plot (train_data)
+        # plt.plot (test_data)
+        # plt.plot (pred)
+        #
+        # # 目标值预测
+        # pred_inputs = normaled_test_data[-train_window:].tolist ()
+        # actual_predictions_fore = model_predict (4, pred_inputs, scaler_test)
+        # forecast_data[index] = actual_predictions_fore
+        # plt.plot (forecast_data[index])
+        # plt.savefig (index + '.png')
+        # plt.show ()
